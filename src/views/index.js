2ed3b22fae87ff8b8726539134cc715ca7aad22f
Chesare22 <Chesare22@github.com>
2020-07-17 00:21:13 -0500

    Add more content

--- src/views/index.js
+++ src/views/index.js
@@ -1,1 +1,8 @@
+// Styles
 import 'src/assets/sass/common.scss';
+import 'src/assets/sass/photo-gallery.scss';
+
+// Images
+import 'src/assets/images/handlebars_logo.png';
+import 'src/assets/images/webpack_logo.png';
+import 'src/assets/images/sass_logo.png';
